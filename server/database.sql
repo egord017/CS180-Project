144957c0d7d5ff58b18f8aee79709573f7dbc152
wilki52 <williamk3210@gmail.com>
2025-02-05 01:52:13 -0800

    Merge database.sql into init.sql

--- server/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE writersblock;
-
-CREATE TABLE users(
-    userID uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    userName VARCHAR(255) NOT NULL,
-    userEmail VARCHAR(255) NOT NULL,
-    userPassword VARCHAR(255) NOT NULL
-);
--- server/init.sql
+++ server/init.sql
@@ -1,16 +1,18 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS groups CASCADE;
-DROP TABLE IF EXISTS users_groups CASCADE;
-DROP TABLE IF EXISTS channels CASCADE;
-DROP TABLE IF EXISTS threads CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
+\c postgres
+
+DROP DATABASE IF EXISTS writers_db;
 
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    bio TEXT
+
+CREATE DATABASE writers_db;
+
+\c writers_db
+
+CREATE TABLE users(
+    userID uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    userName VARCHAR(255) NOT NULL,
+    userEmail VARCHAR(255) NOT NULL,
+    userPassword VARCHAR(255) NOT NULL
 );
 
 
@@ -22,7 +24,7 @@ CREATE TABLE groups (
 
 CREATE TABLE users_groups (
     group_id INT REFERENCES groups(id),
-    user_id INT REFERENCES users(id),
+    user_id INT REFERENCES users(userID),
     role_id INT,
     PRIMARY KEY (group_id, user_id)
 );
@@ -39,24 +41,24 @@ CREATE TABLE channels (
 
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    user_id uuid,
     channel_id INTEGER,
     title TEXT NOT NULL,
     body TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(userID),
     FOREIGN KEY (channel_id) REFERENCES channels(id)
 );
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    user_id uuid,
     thread_id INTEGER,
     body TEXT,
     FOREIGN KEY (thread_id) REFERENCES threads(id)
 );
-INSERT INTO users(name, password) VALUES ('willy', 'sand');
-INSERT INTO users(name, password) VALUES ('oats', 'sand');
-INSERT INTO users(name, password) VALUES ('fox', 'sand');
+INSERT INTO users(userName, userEmail, userPassword) VALUES ('billy', "wiliaim321@gmail.com", 'sand');
+INSERT INTO users(userName, userEmail, userPassword) VALUES ('oats',  "wiliaim321@gmail.com",'sand');
+INSERT INTO users(userName, userEmail, userPassword) VALUES ('fox',  "wiliaim321@gmail.com",'sand');
 
 INSERT INTO groups(id, name, description) VALUES (1, 'poet''s society', 'Writing Workshop for poets');
 INSERT INTO groups(id, name, description) VALUES (2, 'Short Story Group', 'Writing Workshop for short story writers');
