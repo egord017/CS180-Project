b5689937e7631134bc9bd70941ec2c582cd2e80a
anikkasood <asood008@ucr.edu>
2025-02-10 16:02:13 -0800

    started on authorization frontend. need to add registration ui & fix more formatting stuff

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,11 @@
 import React, {Fragment, useState} from "react";
 import {Link} from "react-router-dom";
+import './Login.css';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 
 const Login = ({setAuth}) => {
 
@@ -45,15 +51,26 @@ const Login = ({setAuth}) => {
     }
 
     return (
-        <Fragment>
-            <h1 className="text-center my-5">Login</h1>
+      <div className="page my_fullscreen_div">
+            <div className="login-container Container">
+             
+            <h1 id = "login-header">Log In</h1>
             <form onSubmit = {onSubmitForm}>
-                <input type="email" name ="email" placeholder= "Email" className="form-control my-3" value={email} onChange={e => onChange(e)}/>
-                <input type="password" name ="password" placeholder= "Password" className="form-control my-3" value={password} onChange={e => onChange(e)}/>
+            <h2>EMAIL</h2>
+                <input type="email" name ="email" placeholder= "" className="form-control my-3" value={email} onChange={e => onChange(e)}/>
+            <h2>PASSWORD</h2>
+                <input type="password" name ="password" placeholder= "" className="form-control my-3" value={password} onChange={e => onChange(e)}/>
                 <button className= "btn btn-success w-100">Submit</button>
             </form>
             <Link to="/register">Register</Link>
-        </Fragment>
+
+            <Container maxWidth="sm">
+            </Container>
+            </div>
+            
+             </div>
+
+   
     );
 };
 
