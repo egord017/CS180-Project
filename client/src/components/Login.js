b08a70ec230f35f45bfab210db1f3e3c17f8decf
anikkasood <asood008@ucr.edu>
2025-02-11 12:34:11 -0800

    formatting of login page done - need to add header

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,6 +4,7 @@ import './Login.css';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
 
@@ -52,23 +53,37 @@ const Login = ({setAuth}) => {
 
     return (
       <div className="page my_fullscreen_div">
-            <div className="login-container Container">
-             
+         {/* <CssBaseline /> */}
+         <Container disableGutters maxWidth={false}>
+            <div className="login-container  my_fullscreen_div">
+            
             <h1 id = "login-header">Log In</h1>
             <form onSubmit = {onSubmitForm}>
             <h2>EMAIL</h2>
                 <input type="email" name ="email" placeholder= "" className="form-control my-3" value={email} onChange={e => onChange(e)}/>
             <h2>PASSWORD</h2>
                 <input type="password" name ="password" placeholder= "" className="form-control my-3" value={password} onChange={e => onChange(e)}/>
-                <button className= "btn btn-success w-100">Submit</button>
+                <button className= "btn btn-success w-100"><h2>SUBMIT</h2></button>
             </form>
-            <Link to="/register">Register</Link>
 
-            <Container maxWidth="sm">
-            </Container>
-            </div>
+          
             
+            </div>
+            </Container>
+            <Container disableGutters maxWidth={false}>
+            <div className="register-container my_fullscreen_div">
+              <h2 id = "register-header">Welcome to Writer's Block!</h2>
+              <h2 id = "register-subheader">Don't Have An Account?</h2>
+             
+             
+              <Link to="/register">
+              <button className= "sign-up-btn">SIGN UP</button>
+              </Link>
+            
+            </div>
+            </Container>
              </div>
+             
 
    
     );
