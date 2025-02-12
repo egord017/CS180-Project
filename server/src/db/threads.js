b3adc139b88be7fa5f8882ae48173201a09a965d
aphan079 <aphang355@gmail.com>
2025-02-11 23:59:25 -0800

    finished db/threads.

---
+++ server/src/db/threads.js
@@ -0,0 +1,36 @@
+const pool = require('../../db.js');
+
+async function get_threads() {
+    try {
+        const results = await pool.query("SELECT * FROM threads");
+        return results.rows;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function get_thread(thread_id) {
+    try {
+        const query = "SELECT * FROM threads WHERE id = $1";
+        const result = await pool.query(query, thread_id);
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function get_comments_from_thread(thread_id) {
+    try {
+        const query = "SELECT * FROM comments WHERE thread_id = $1";
+        const result = await pool.query(query, thread_id);
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {
+    get_threads,
+    get_thread,
+    get_comments_from_thread
+};
