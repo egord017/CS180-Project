4305b780451e51be4fe593d15aa6865eb89216c2
aphan079 <aphang355@gmail.com>
2025-02-14 02:22:01 -0800

    added morgan library to help debug along with postman

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 require('dotenv').config();
 const db = require('./db.js');
+const morgan = require("morgan");
+const threadsRoutes = require("./src/routes/threads");
 
 const app = express();
 const cors = require("cors");
@@ -11,12 +13,14 @@ const cors = require("cors");
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev")); //added morgan for api testing
 
 //routes
 app.use("/auth", require("./src/routes/jwtAuth.js")); //login and register route
 
 app.use("/dashboard", require("./src/routes/dashboard.js")); //dashboard route
-app.use('/groups', require('./src/routes/groups.js'));
+app.use('/groups', require('./src/routes/groups.js')); //groups route
+app.use("/threads", require("./src/routes/threads.js")); //threads route
 
 
 //temporary test, will remove lol
