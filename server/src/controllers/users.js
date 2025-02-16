e66d9d279f2fb375117409addc0a9451350ac016
Brandon Shin <bmshin02@gmail.com>
2025-02-16 12:45:46 -0800

    Update #5 - updated controller for users

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,30 +1,31 @@
-const users_db = require('../db/threads.js');
+const users_db = require('../db/users.js');
 
 async function get_users(req, res) {
-    // const results = await threads_db.get_threads();
-    // res.send(results);
+    const results = await users_db.get_users();
+    res.send(results);
 }
 
 async function get_user(req, res) {
-    // const results = await threads_db.get_threads();
-    // res.send(results);
+    const params = Object.values(req.params);
+    const results = await users_db.get_user(params);
+    res.send(results);
 }
 
-async function get_user_groups(req, res) {
-    // const results = await threads_db.get_threads();
-    // res.send(results);
+async function get_comments_from_user(req, res) {
+    const params = Object.values(req.params);
+    const results = await users_db.get_comments_from_user(params);
+    res.send(results);
 }
 
-async function get_users_posts(req, res) {
-    // const results = await threads_db.get_threads();
-    // res.send(results);
+async function get_threads_from_user(req, res) {
+    const params = Object.values(req.params);
+    const results = await users_db.get_threads_from_user(params);
+    res.send(results);
 }
 
 module.exports = {
-    // get_threads,
-    // get_thread,
-    // get_comments_from_thread,
-    // create_thread,
-    // update_thread,
-    // delete_thread
+    get_users,
+    get_user,
+    get_comments_from_user,
+    get_threads_from_user
 };
--- server/src/db/users.js
+++ server/src/db/users.js
@@ -9,16 +9,37 @@ async function get_users() {
     }
 }
 
-async function get_user() {
+async function get_user(user_id) {
     try {
-        const results = await pool.query("SELECT * FROM users WHERE id = $1");
+        const query = "SELECT * FROM threads WHERE id = $1";
+        const results = await pool.query(query, user_id);
         return results.rows;
     } catch (err) {
         console.error(err);
     }
 }
 
-async function get_user_groups() {
+async function get_comments_from_user(user_id) {
+    try {
+        const query = "SELECT * FROM comments WHERE user_id = $1";
+        const results = await pool.query(query, user_id);
+        return results.rows;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function get_threads_from_user(user_id) { // aka get posts from user
+    try {
+        const query = "SELECT * FROM threads WHERE user_id = $1";
+        const results = await pool.query(query, user_id);
+        return results.rows;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function get_groups_from_user() {
     // try {
     //     const results = await pool.query("SELECT * FROM users WHERE id = $1");
     //     return results.rows;
@@ -27,11 +48,12 @@ async function get_user_groups() {
     // }
 }
 
-async function get_user_posts() {
-    // try {
-    //     const results = await pool.query("SELECT * FROM users WHERE id = $1");
-    //     return results.rows;
-    // } catch (err) {
-    //     console.error(err);
-    // }
-}
+
+
+module.exports = {
+    get_users,
+    get_user,
+    get_comments_from_user,
+    get_threads_from_user
+    //get_groups_from_user
+};
