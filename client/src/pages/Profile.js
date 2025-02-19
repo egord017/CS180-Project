8b5c9a65eeb7abb5d602a92d28e6e7300d0f4a71
isabellecelo <isabelle.celo04@gmail.com>
2025-02-18 18:57:05 -0800

    test

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,17 @@
+import React, {Fragment, useState} from "react";
+import {Link} from "react-router-dom";
+import './Profile.css';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+return (
+  <div className="profilePic">
+  </div>
+
+
+);
+
+export default Profile;
