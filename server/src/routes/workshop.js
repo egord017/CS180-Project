9cc28b75057e31e0fc2e153caacca1b4c392eb74
Brandon Shin <bmshin02@gmail.com>
2025-02-24 22:54:58 -0800

    Update #31

--- server/src/routes/workshop.js
+++ server/src/routes/workshop.js
@@ -3,7 +3,6 @@ const workshopController = require("../controllers/workshop.js");
 
 const router = express.Router();
 
-//Get all users user_id for testing
 router.get("/", workshopController.get_users);
 
 module.exports = router;
