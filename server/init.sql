3e9caa148353c8010661c46992a7425eef628d89
wilki52 <williamk3210@gmail.com>
2025-03-10 23:07:54 -0700

    Add bob (password:bob) with groups

--- server/init.sql
+++ server/init.sql
@@ -177,6 +177,12 @@ VALUES (1, (SELECT userID FROM users WHERE userName = 'faye'), 1);
 INSERT INTO users_groups (group_id, user_id, role_id) 
 VALUES (2, (SELECT userID FROM users WHERE userName = 'faye'), 2);
 
+-- BOB'S GROUPS
+INSERT INTO users_groups (group_id, user_id, role_id) 
+VALUES (1, (SELECT userID FROM users WHERE userName = 'bob'), 0);
+INSERT INTO users_groups (group_id, user_id, role_id) 
+VALUES (2, (SELECT userID FROM users WHERE userName = 'bob'), 1);
+
 -- CHANNELS -----------------------------------------------
 -- 1
 INSERT INTO channels (group_id, name, description)
