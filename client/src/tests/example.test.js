837874e2cf42c6a8a4aa693373324470fb27ac43
wilki52 <williamk3210@gmail.com>
2025-03-12 19:09:03 -0700

    Add example

---
+++ client/src/tests/example.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {http, HttpResponse} from 'msw'
+import {setupServer} from 'msw/node'
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Fetch from '../fetch'
+
+import ThreadPostForm from "../src/pages/ThreadPostForm";
+
+//https://legacy.reactjs.org/docs/testing-recipes.html#setup--teardown
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
--- client/tests/component-test/example.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import {http, HttpResponse} from 'msw'
-import {setupServer} from 'msw/node'
-import {render, fireEvent, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Fetch from '../fetch'
