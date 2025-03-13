b9b33cc0738d2c857974c1cc962da53c62bb6e6a
wilki52 <williamk3210@gmail.com>
2025-03-13 02:59:31 -0700

    Create auth.spec.js

---
+++ client/e2e-tests/auth.spec.js
@@ -0,0 +1,40 @@
+import { test, expect, describe } from '@playwright/test';
+
+
+describe('Successful Registration Workflow', ()=>{
+  test('user goes to register page', async ({page})=>{
+    await page.goto('http://localhost:3000/login');
+    await page.locator('.sign-up-btn').click();
+    await expect(page).toHaveURL('http://localhost:3000/register');
+  });
+
+  test('user submits registration form', async ({page})=>{
+    await page.goto('http://localhost:3000/register');
+    await page.locator('input[name="username"]').fill(`${Date.now()}`);
+    await page.locator('input[name="email"]').fill(`${Date.now()}@gmail.com`);
+    await page.locator('input[name="password"]').fill(`${Date.now()}`);
+    await page.locator('.btn').click();
+    await expect(page).toHaveURL('http://localhost:3000/dashboard');
+
+  });
+});
+
+
+describe('Successful Login Workflow', ({page})=>{
+
+    
+});
+//login
+// describe('Authentication', ()=>{
+//     test('user logs in', async ({ page }) => {
+//         await page.goto('http://localhost:3000/login');
+//         await page.fill('input[name="email"]', 'testuser@example.com');
+//         await page.fill('input[name="password"]', 'password123');
+//         await page.click('button[type="submit"]');
+//         await expect(page).toHaveURL('http://localhost:3000/dashboard');
+//       });
+
+// });
+
+//logout
+
