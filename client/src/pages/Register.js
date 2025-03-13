0bf8163526b0e82ee6ad5471fc9b107bc27cf066
wilki52 <williamk3210@gmail.com>
2025-03-12 23:56:01 -0700

    Implement clientside/serverside register validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,6 +3,12 @@ import {Link} from "react-router-dom";
 import './Register.css';
 
 const Register = ({setAuth}) => {
+
+    const [regError, setRegError] = useState({
+        username:"",
+        email:"",
+        password:""
+    });
     
     const [inputs, setInputs] = useState({
         username: "",
@@ -23,27 +29,44 @@ const Register = ({setAuth}) => {
 
             const body = {username, email, password};
 
+            let errorTriggered = false;
             //clientside validation
             if (username.length===0){
-                console.log("Cannot be empty");
+                setRegError((prevState)=>({...prevState, username:"Username required."}));
+                errorTriggered = true;
+            }
+            else if (username.length<3){
+                setRegError((prevState)=>({...prevState, username:"Username is too short (min:3)"}));
+                errorTriggered = true;
+
+            }
+            else if (username.length>15){
+                setRegError((prevState)=>({...prevState, username:"Username is too long (max:15)"}));
+                errorTriggered = true;
+
+            }
+            else{
+                setRegError((prevState)=>({...prevState, username:""}));
+
+            }
+            if(password.length ===0){
+                setRegError((prevState)=>({...prevState, password:"Password required"}));
+                errorTriggered = true;
+
+            }
+            else if (password.length <3){
+                setRegError((prevState)=>({...prevState, password:"Password too short (min:3)"}));
+                errorTriggered = true;
+
+            }
+            else{
+                console.log("??")
+                setRegError((prevState)=>({...prevState, password:""}));
+                
+            }
+            if (errorTriggered===true){
                 return;
             }
-            if (username.length<3){
-                console.log("Username is too short (min:3)");
-                return;
-            }
-            if (username.length>15){
-                console.log("Username is too long (max:15)");
-                return;
-            }
-            if (password.length ===0){
-                console.log();
-                return;
-            }
-            if (password.length <3){
-                return;
-            }
-            
 
             const response = await fetch("http://localhost:5000/auth/register", {
                 method: "POST",
@@ -53,7 +76,18 @@ const Register = ({setAuth}) => {
 
             const parseResponse = await response.json();
 
-            //console.log(parseResponse);
+            console.log(parseResponse);
+            if (!response.ok){
+                if (parseResponse==="Email already exists"){
+                    setRegError((prevState)=>({...prevState, email:parseResponse}));
+                    return;
+                }
+                else if (parseResponse==="Username is taken"){
+                    setRegError((prevState)=>({...prevState, username:parseResponse}));
+                    return;
+                }
+
+            }
 
             if(parseResponse.token){
                 localStorage.setItem("token", parseResponse.token);
@@ -86,11 +120,16 @@ const Register = ({setAuth}) => {
         <form onSubmit = {onSubmitForm}>
             <h2>USERNAME</h2>
                 <input type="text" name="username" className = "form-control my-3" value={username} onChange={e => onChange(e)} required/> 
+                {regError.username && <p>{regError.username}</p>}
             <h2>EMAIL</h2>
                 <input type="email" name="email" className = "form-control my-3" value={email} onChange={e => onChange(e)} required/>
+                {regError.email && <p>{regError.email}</p>}
+
             <h2>PASSWORD</h2>
-                <input type="password" name="password" className = "form-control my-3" value={password} onChange={e => onChange(e)} required/>   
-            <button className= "btn btn-success w-100"><h2>SUBMIT</h2></button>""
+                <input type="password" name="password" className = "form-control my-3" value={password} onChange={e => onChange(e)} required/>
+                {regError.password && <p>{regError.password}</p>}
+                {console.log(regError)}
+            <button className= "btn btn-success w-100"><h2>SUBMIT</h2></button>
         </form>
         <h2 id = "already-have-account">Already have an account? <a href="/login">Return to Login</a></h2>
         </div>
