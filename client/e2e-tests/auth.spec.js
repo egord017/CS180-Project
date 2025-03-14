aa700aa530217ea2504f3664fb03054e28df9ca2
aphan079 <aphang355@gmail.com>
2025-03-14 02:32:34 -0700

    54 tests total for playwright. working on doc now

--- client/e2e-tests/auth.spec.js
+++ client/e2e-tests/auth.spec.js
@@ -31,25 +31,22 @@ describe('Successful Login Workflow', ()=>{
 });
 
 describe('Successful Group Workflow', ()=>{
-  test('user goes to groups page', async ({page})=>{
+  test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/login');
     await page.locator('input[name="email"]').fill('bob@gmail.com');
     await page.locator('input[name="password"]').fill('bob');
     await page.locator('.btn.btn-success.w-100').click();
     await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
+  });
+
+
+  test('user goes to groups page', async ({page})=>{
     await page.getByRole('link', { name: 'Groups' }).click();
     await expect(page).toHaveURL('http://localhost:3000/groups');
 
   });
 
   test('user clicks on a group', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
     await page.getByRole('link', { name: 'Groups' }).click();
     await expect(page).toHaveURL('http://localhost:3000/groups');
 
@@ -58,12 +55,6 @@ describe('Successful Group Workflow', ()=>{
   });
 
   test('user creates a group', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
     await page.getByRole('link', { name: 'Groups' }).click();
     await expect(page).toHaveURL('http://localhost:3000/groups');
 
@@ -72,31 +63,26 @@ describe('Successful Group Workflow', ()=>{
 
     await page.locator('#groupname').fill('New Test Group');
     await page.locator('#groupdescription').fill('New test group for testing.');
-    await page.locator('#submit').click();
-
-
+    await page.locator('#submit-group').click();
   });
+
 });
 
-describe('Successful User Post Workflow', ()=>{
-  test('user goes to their group', async ({page})=>{
+describe('Successful User Thread Workflow', ()=>{
+  test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/login');
     await page.locator('input[name="email"]').fill('bob@gmail.com');
     await page.locator('input[name="password"]').fill('bob');
     await page.locator('.btn.btn-success.w-100').click();
     await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
-    await page.locator('.group-card').first().click();
-    await expect(page).toHaveURL('http://localhost:3000/group/1');
   });
 
-  test('user makes a post', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
+  test('user navigates to a thread', async ({page})=>{
+    await page.locator('.thread-card').first().click();
+    await expect(page).toHaveURL(/\/thread\/\d+$/);
+  });
+
+  test('user makes a a new thread', async ({page})=>{
     await page.locator('.group-card').first().click();
     await expect(page).toHaveURL('http://localhost:3000/group/1');
 
@@ -104,86 +90,170 @@ describe('Successful User Post Workflow', ()=>{
     await page.locator('#\\:r9\\:').fill('test123');
     await page.locator('.button-post').click();
   });
-})
-
-describe('Successful User Thread Workflow', ()=>{
-  test('user makes a new thread from groups', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
-    await page.locator('.group-card').first().click();
-    await expect(page).toHaveURL('http://localhost:3000/group/1');
-
-    await page.getByRole('button', { name: 'Create New' }).click();
-    await page.locator('#channel_name').fill('Test chjanne;l.');
-    await page.locator('form #description').fill('Test Desc');
-    await page.getByRole('button', { name: 'Create Channel' }).click();
-    await page.reload();
-    
-  });
-  
-  // test('user deletes a comment on a thread', async ({page})=>{
-  //   await page.goto('http://localhost:3000/login');
-  //   await page.locator('input[name="email"]').fill('bob@gmail.com');
-  //   await page.locator('input[name="password"]').fill('bob');
-  //   await page.locator('.btn.btn-success.w-100').click();
-  //   await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
-  //   await page.locator('.thread-card').first().click();
-  //   await expect(page).toHaveURL(/\/thread\/\d+$/);
-  //   await page.locator('.comment-item .del-btn').first().click();
-  //   await page.reload();
-  // });
 
   test('user comments on a thread', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
     await page.locator('.thread-card').first().click();
     await expect(page).toHaveURL(/\/thread\/\d+$/);
 
     await page.locator('.reply-button').click();
     await page.locator('#comment-body').fill('Test comment');
     await page.getByRole('button', { name: 'Post Comment' }).click();
-    await page.reload();
-    await page.locator('.reply-button').click();
-    await page.locator('#comment-body').fill('Test comment2');
-    await page.getByRole('button', { name: 'Post Comment' }).click();
-    await page.locator('.reply-button').click();
-    await page.locator('#comment-body').fill('Test comment3');
-    await page.getByRole('button', { name: 'Post Comment' }).click();
-    await page.locator('.reply-button').click();
-    await page.locator('#comment-body').fill('Test comment4');
-    await page.getByRole('button', { name: 'Post Comment' }).click();
-  });
 
-  test('user deletes a thread', async ({page})=>{
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').fill('bob@gmail.com');
-    await page.locator('input[name="password"]').fill('bob');
-    await page.locator('.btn.btn-success.w-100').click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
-    await page.locator('.thread-card').first().click();
-    await expect(page).toHaveURL(/\/thread\/\d+$/);
-    await page.locator('.delete-btn').first().click();
-    await expect(page).toHaveURL('http://localhost:3000/dashboard');
-    
   });
 })
 
+describe('Successful User Profile Workflow', ()=>{
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:3000/login');
+        await page.locator('input[name="email"]').fill('bob@gmail.com');
+        await page.locator('input[name="password"]').fill('bob');
+        await page.locator('.btn.btn-success.w-100').click();
+        await expect(page).toHaveURL('http://localhost:3000/dashboard');
+    });
+
+    test('user goes to their profile', async ({page})=>{
+      await page.getByText('Profile').click();
+      await expect(page).toHaveURL('http://localhost:3000/profile/bob');
+    });
+
+    test('user clicks on a group they joined from their profile', async ({page})=>{
+      await page.getByText('Profile').click();
+      await expect(page).toHaveURL('http://localhost:3000/profile/bob');
+
+      await page.getByText("Poet's Society").click();
+      await expect(page).toHaveURL('http://localhost:3000/group/1');
+    });
+
+  test("user goes to a group members' profile", async ({page})=>{
+    await page.getByRole('link', { name: 'Groups' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/groups');
+
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Group Members' }).click();
+    await page.locator('ul li a[href^="/profile/"]').first().click();
+    await expect(page).toHaveURL(/\/profile\/\w+$/);
+  });
+});
+
+describe('Successful User Workshop Workflow', ()=>{
+  test.beforeEach(async ({ page }) => {
+      await page.goto('http://localhost:3000/login');
+      await page.locator('input[name="email"]').fill('bob@gmail.com');
+      await page.locator('input[name="password"]').fill('bob');
+      await page.locator('.btn.btn-success.w-100').click();
+      await expect(page).toHaveURL('http://localhost:3000/dashboard');
+  });
+
+  test('user goes to the workshop tab', async ({page})=>{
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Workshops' }).click();
+    await page.locator('.workshop-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1');
+
+  });
+
+  test('user clicks on a short story', async ({page})=>{
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Workshops' }).click();
+    await page.locator('.workshop-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1');
+
+    await page.locator('.threads-container').first().click();
+  });
+
+  test('user clicks on a short story and makes a critique', async ({page})=>{
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Workshops' }).click();
+    await page.locator('.workshop-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1');
+
+    await page.getByText('Notes From The Ground CH2').click();
+    await expect(page).toHaveURL(/\/workshop-thread\/\d+$/);
+    await page.reload();
+
+    await page.getByRole('button', { name: 'Critique' }).first().click();
+    await expect(page).toHaveURL(/\/workshop-thread\/\d+\/submit$/);
+
+    await page.locator('#opening').fill('This is my test critique.');
+    await page.locator('#body').fill('test body critique.');
+    await page.locator('#closing').fill('closing test critique.');
+    await page.getByRole('button', { name: 'Post Thread' }).click();
+    await page.reload();
+ });
+ 
+ 
 
 
-//logout
+  test('user edits the text for a critque', async ({page})=>{
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Workshops' }).click();
+    await page.locator('.workshop-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1');
+
+    await page.getByText('Notes From The Ground CH2').click();
+    await expect(page).toHaveURL(/\/workshop-thread\/\d+$/);
+    await page.reload();
+
+    await page.waitForSelector('button:has-text("Critique")', { state: 'visible', timeout: 5000 });
+    await page.getByRole('button', { name: 'Critique' }).first().click();
+    await expect(page).toHaveURL(/\/workshop-thread\/\d+\/submit$/);
+
+    await page.evaluate(() => {
+      const editable = document.querySelector('.passage-body ins[contenteditable="true"]');
+      if (editable) editable.innerText = 'TEST EDIT';
+    });
+
+    await page.locator('.passage-body').click();
+  
+    await page.locator('.passage-body ins[contenteditable="true"]').click();
+    await page.keyboard.type('This is my new test edit.');
+    
+    await page.locator('#opening').fill('Edit test critique.');
+    await page.locator('#body').fill('Edit body critique.');
+    await page.locator('#closing').fill('Edit closing critique.');
+    await page.getByRole('button', { name: 'Post Thread' }).click();
+    await expect(page).toHaveURL(/\/critique\/\d+$/);
+  });
+
+
+
+  test('user makes a new post for critique', async ({page})=>{
+    await page.locator('.group-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/group/1');
+
+    await page.getByRole('tab', { name: 'Workshops' }).click();
+    await page.locator('.workshop-card').first().click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1');
+
+    await page.getByRole('button', { name: 'Post'}).click();
+    await expect(page).toHaveURL('http://localhost:3000/workshop/1/submit');
+    await page.reload();
+
+    await page.locator('#title').fill('test title.');
+    await page.locator('#context').fill('test context.');
+    await page.locator('#preference').fill('test critque guidance');
+    await page.locator('#post_body').fill('test post bodye');
+    await page.locator('#passage_body').fill('test passage body');
+
+    await page.getByRole('button', { name: 'Post Thread' }).click();
+    await expect(page).toHaveURL(/\/workshop-thread\/\d+$/);
+  });
+
+});
+
 describe('Successful Logout Workflow', ()=>{
   
-  test('user logins to writers block', async ({page})=>{
+  test('user logouts from writers block', async ({page})=>{
     await page.goto('http://localhost:3000/login');
     await page.locator('input[name="email"]').fill('bob@gmail.com');
     await page.locator('input[name="password"]').fill('bob');
